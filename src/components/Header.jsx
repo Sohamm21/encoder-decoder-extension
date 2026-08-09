e1a409d2102ea2b845d3da2d2701bdadc8e2ade6
Soham Kumbhar <sohamkumbhar73@gmail.com>
2026-08-09 20:45:08 +0530

    feat: add application header component and branding assets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,7 @@ import "./index.css";
 const Header = () => {
   return (
     <div className="header">
-      <img
-        src="https://res.cloudinary.com/dyrv985gb/image/upload/v1767114288/encoder-decoder/Gemini_Generated_Image_zop6xzop6xzop6xz_eosjsj.png"
-        className="header-icon"
-        loading="lazy"
-      />
+      <img src="/logo.png" className="header-icon" loading="lazy" />
       <div className="header-text">
         <span className="medium-font">EncodeDecode Craft</span>
         <span className="guide-text">Utility Tool</span>
